2d5d30700cbd8ddecbd3a9df2183a8602a4d71dc
EbrarMutlu <71319071+EbrarMutlu@users.noreply.github.com>
2022-05-28 23:17:56 +0300

    Update WordleGame.java

--- src/WordleGame.java
+++ src/WordleGame.java
@@ -137,69 +137,8 @@ public class WordleGame implements ActionListener{ //implementing the actionlist
 	
 	
 	
-<<<<<<< Updated upstream
 	
 	
-	
-	public WordleGame() { // constructor, initializing
-		
-		gameFrame = new JFrame("Wordle Game BAU Ver");
-		gameFrame.setSize(300, 300);
-		gameFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		gameFrame.setLayout(new GridLayout(8, 1)); // 6 for tries, 1 for userinput
-		gameFrame.setVisible(true);
-		gameFrame.setLocationRelativeTo(null); //puts it in the center of our system
-		
-		
-		time = new showTime();
-		
-		gameFrame.add(time);
-		
-		
-		for (int i = 0; i < 6; i++) {  //initializng the wordpanel array
-			wordPanelArray[i] = new WordPanel();
-			gameFrame.add(wordPanelArray[i]);
-=======
-
-	
-//public WordleGame() { // constructor, initializing
-//
-//	gameFrame = new JFrame("Wordle Game BAU Ver");
-//	gameFrame.setSize(300, 300);
-//	gameFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-//	gameFrame.setLayout(new GridLayout(8, 1)); // 6 for tries, 1 for userinput
-//	gameFrame.setVisible(true);
-//	gameFrame.setLocationRelativeTo(null); //puts it in the center of our system
-//
-//
-//	time = new showTime();
-//
-//	gameFrame.add(time);
-//
-//
-//	for (int i = 0; i < 6; i++) {  //initializng the wordpanel array
-//		wordPanelArray[i] = new WordPanel();
-//		gameFrame.add(wordPanelArray[i]);
-//	}
-//
-//
-//	userPanel = new UserPanel(); //initializing the userpanel
-//	userPanel.getEnterButton().addActionListener(this);
-//
-//	gameFrame.add(userPanel);
-//
-//
-//	gameFrame.revalidate();
-//
-//	wordleString = getWordleString();
-//	System.out.println("Word: " + wordleString);
-//
-//
-//
-//
-//}
-//
-
 	public String obtainValidUserWord (List<String> wordList) {
         Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
         String userWord = myScanner.nextLine();  // Read user input
@@ -237,7 +176,6 @@ public class WordleGame implements ActionListener{ //implementing the actionlist
 					return;
 			 }
 			 count++;
->>>>>>> Stashed changes
 		}
 			 if(userWord.length() > 5 || userWord.length() < 5 ) {
 				 JOptionPane.showMessageDialog(null, "Word should  be 5 letter word, try again","!", JOptionPane.INFORMATION_MESSAGE);
@@ -254,23 +192,6 @@ public class WordleGame implements ActionListener{ //implementing the actionlist
 		
 		CorrectLetterCount=0;	
 		
-<<<<<<< Updated upstream
-		userPanel = new UserPanel(); //initializing the userpanel
-		userPanel.getEnterButton().addActionListener(this);
-		
-		gameFrame.add(userPanel);
-		
-		
-		gameFrame.revalidate();
-		
-		wordleString = getWordleString();
-		System.out.println("Word for the day : " + wordleString);
-		
-		
-		
-		
-	 }
-=======
 	}
 	
 	private void clearAllPanels() {
@@ -283,61 +204,11 @@ public class WordleGame implements ActionListener{ //implementing the actionlist
 	public WordPanel getActivePanel() {
 		return this.wordPanelArray[count];
 	}
->>>>>>> Stashed changes
 
 	
 	public String getWordleString() {
 		Path path = Paths.get("Words.txt");
 
-<<<<<<< Updated upstream
-	public String obtainValidUserWord (List<String> wordList) {
-        Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
-        String userWord = myScanner.nextLine();  // Read user input
-        userWord = userWord.toLowerCase(); // covert to lowercase
-
-        // check the length of the word and if it exists
-        while ((userWord.length() != 5) || !(wordList.contains(userWord))) {
-            if ((userWord.length() != 5)) {
-                System.out.println("The word " + userWord + " does not have 5 letters.");
-            } else {
-                System.out.println("The word " + userWord + " is not in the word list.");
-            }
-            // Ask for a new word
-            System.out.println("Please, submit a new 5-letter word.");
-            
-            userWord = myScanner.nextLine();
-        }
-        return userWord;
-    }
-	
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		
-		
-		String userWord = this.userPanel.getUserInput().getText();
-
-		if (userWord.length() > 4) {
-			 isWordleWordEqualTo(userWord.trim().toUpperCase());
-			 
-			}
-		
-		count++;	
-		
-	}
-	
-	
-	
-	public WordPanel getActivePanel() {
-		return this.wordPanelArray[count];
-	}
-
-	
-	public String getWordleString() {
-		Path path = Paths.get("C:\\Users\\LENOVO\\eclipse-workspace\\WordleBAU\\Words.txt");
-
 		
 		List<String> wordList = new ArrayList<>();
 		try {
@@ -357,36 +228,12 @@ public class WordleGame implements ActionListener{ //implementing the actionlist
 		String[] userWordsArray = userWord.split("");
 		List<Boolean> wordMatchesList = new ArrayList<>();
 
-=======
-		
-		List<String> wordList = new ArrayList<>();
-		try {
-			wordList = Files.readAllLines(path);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Random random = new Random();
-		int position = random.nextInt(wordList.size());
-		return wordList.get(position).trim().toUpperCase();
-	}
-    
-
-	private boolean isWordleWordEqualTo(String userWord) {
-		List<String> wordleWordsList = Arrays.asList(wordleString.split(""));
-		String[] userWordsArray = userWord.split("");
-		List<Boolean> wordMatchesList = new ArrayList<>();
-
->>>>>>> Stashed changes
 		for (int i = 0; i < 5; i++) {
 			if (wordleWordsList.contains(userWordsArray[i])) {
 				if (wordleWordsList.get(i).equals(userWordsArray[i])) {
 					getActivePanel().setPanelText(userWordsArray[i], i, Color.GREEN);
 					wordMatchesList.add(true);
-<<<<<<< Updated upstream
-=======
 					CorrectLetterCount++;
->>>>>>> Stashed changes
 				} else {
 					getActivePanel().setPanelText(userWordsArray[i], i, Color.YELLOW);
 					wordMatchesList.add(false);
